ebf0415fedda229792c170546abc17cff2748de2
GalloSimone <gallosimonecz@gmail.com>
2024-10-14 15:21:06 +0200

    add sconto

--- js/script.js
+++ js/script.js
@@ -7,13 +7,20 @@
 //- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
 
 //!RACCOLTA DATI
+//CHIEDO DISTANZA E ETÀ
 const distance = parseInt(
     prompt("Inserisci il numero di km che vuoi percorrere")
 );
-
 const age = parseInt(
     prompt("inserire età del passeggiero")
-)
+);
+//INSERISCO PREZZO PER IL SINGOLO KM
+const priceForKm = 0.21;
+
+//INSERISCO GLI SCONTI UNDER 18 E OVER 65
+const discountPercentageUnder18 = 20
+
+const discountPercentageOver65 = 40
 
 
 
